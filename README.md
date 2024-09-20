9ba4314cc20465e7fd7dcf5cc11c08c258f7a956
noah227 <noah227@foxmail.com>
2024-09-20 15:49:58 +0800

    chore: 优化提示；更新说明文档

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 ﻿# 复制文件内容
 
-在项目管理器右键 > 复制文件内容
+复制文件内容到剪贴板
+
+## 使用
+
+> 正常使用
+
+![](https://mp-77dc03ae-7084-429e-8b0f-4d540ae4a430.cdn.bspapp.com/images/hx-copy-file-content/1.gif)
+
+> 超过大小限制的提示
+
+![](https://mp-77dc03ae-7084-429e-8b0f-4d540ae4a430.cdn.bspapp.com/images/hx-copy-file-content/2.gif)
+
+## 配置
+
+工具 > 设置 > 插件配置
+
+![](https://mp-77dc03ae-7084-429e-8b0f-4d540ae4a430.cdn.bspapp.com/images/hx-copy-file-content/1.jpg)
--- extension.js
+++ extension.js
@@ -2,7 +2,21 @@ var hx = require("hbuilderx");
 const fs = require("fs")
 //该方法将在插件激活的时候调用
 function activate(context) {
-	const sizeWarningLimit = 5 * 1024 * 1024
+    const sizeWarningLimit = 5 * 1024 * 1024
+    const getSizeWarningLimit = () => {
+        const _ = hx.workspace.getConfiguration().get("hx-copy-file-content.sizeWarningLimit", sizeWarningLimit)
+        if(_ === 0) return Infinity
+        if(_ < 0 || typeof _ !== "number") {
+            hx.window.showErrorMessage([
+                "<div>【文件内容复制】：<br>",
+                "插件<b>文件过大预警值</b>配置无效，已使用默认的尺寸限制（5MB）",
+                "</div>"
+            ].join(""))
+            return sizeWarningLimit
+        }
+        return _
+    }
+    
 	const copyAction = (fsPath) => {
 		try {
 			hx.env.clipboard.writeText(fs.readFileSync(fsPath, {encoding: "utf8"}))
@@ -17,11 +31,15 @@ function activate(context) {
 		
 		// Byte
 		const size = fs.statSync(fsPath).size
-		if(size > sizeWarningLimit) {
+        const sizeLimit = getSizeWarningLimit()
+		if(size > sizeLimit) {
             const filesize = require("filesize")
 			hx.window.showMessageBox({
 				title: "提示",
-				text: `当前文件过大（${filesize.filesize(size)}），是否继续复制文件内容？`,
+				text: [
+                    `当前文件过大（${filesize.filesize(size, {base: 2})}），是否继续复制文件内容？`,
+                    `可在插件配置里调整大小限制（当前限制：${filesize.filesize(sizeLimit, {base: 2})}）`
+                ].join("\n\n"),
 				buttons: ["取消", "复制"],
 				defaultButton: "取消"
 			}).then(button => {
